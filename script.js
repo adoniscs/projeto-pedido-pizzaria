16a84210043e7417e3b7b09c52ad9fe0c6be73a0
Adonis C. Silveira <desenvolvimento777@gmail.com>
2023-12-22 19:44:33 -0300

    funcionalidades para mostrar modal e dados na modal

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let modalQtd = 1;
 const query = (element) => {
     return document.querySelector(element);
 }
@@ -8,21 +9,53 @@ const queryAll = (element) => {
 pizzaJson.map((item, index) => {
     const pizzaItem = query('.models .pizza-item').cloneNode(true);
 
-    // preencher as informações em pizzaItem
+    // preencher as informações em pizzaItem e na modal
+    pizzaItem.setAttribute('data-key', index);
     //Imagem
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-
     //Preço
     pizzaItem.querySelector('.pizza-item--price').innerHTML = item.price.toLocaleString('pt-br', {
             minimumFractionDigits: 2, style: 'currency', currency: 'BRL'
         }
     );
-
     //Nome
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
-
     //Descrição
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    // fim da inclusão dos dados na tela
+
+    // adicionar o evento de click ao clicar na pizza e abrir a modal
+    pizzaItem.querySelector('.pizza-item a').addEventListener('click', (event) => {
+       event.preventDefault();
+
+       // procurar o elemento mais proximo que tenha a classe .pizza-item
+       let key = event.target.closest('.pizza-item').getAttribute('data-key');
+       modalQtd = 1;
+
+       query('.pizzaBig img').src = pizzaJson[key].img;
+       query('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+       query('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+       query('.pizzaInfo--size.selected').classList.remove('selected');
+       queryAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+           if (sizeIndex === 2) {
+               size.classList.add('selected');
+           }
+           size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+       });
+
+       query('.pizzaInfo--actualPrice').innerHTML = pizzaJson[key].price.toLocaleString('pt-br', {
+           minimumFractionDigits: 2, style: 'currency', currency: 'BRL'
+       });
+
+       query('.pizzaInfo--qt').innerHTML = modalQtd;
+
+       query('.pizzaWindowArea').style.opacity = 0;
+       query('.pizzaWindowArea').style.display = 'flex';
+       setTimeout(() => {
+          query('.pizzaWindowArea').style.opacity = 1;
+       }, 200);
+    });
+    // --- fim modal ---
 
     // mostrar as pizzas na tela
     query('.pizza-area').append(pizzaItem);
